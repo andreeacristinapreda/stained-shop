736ca536e311913518ffd2247e3b3aa3db066d70
andreeacristinapreda <andreea.preda2000>
2020-05-05 20:52:22 +0300

    to delete

---
+++ src/configs/firebase.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnyIGNnYu-ALOxk2a1oQdBEFqQOjRpCXM",
+  authDomain: "stained-shop.firebaseapp.com",
+  databaseURL: "https://stained-shop.firebaseio.com",
+  projectId: "stained-shop",
+  storageBucket: "stained-shop.appspot.com",
+  messagingSenderId: "475386405839",
+  appId: "1:475386405839:web:4dc9cad60e698cd20f68ba"
+  };
+
+  export default firebaseConfig;
+
+  
